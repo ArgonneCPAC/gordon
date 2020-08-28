0ca2941c115596d29dde233a8187614d972c46cb
Andrew Hearin <ahearin@anl.gov>
2020-08-28 09:42:19 -0500

    Added test_kernel_weighted_hist_of_model_gradients function propagating gradients from jax through histograms with numba

--- gordon/tests/test_gordon.py
+++ gordon/tests/test_gordon.py
@@ -1,8 +1,12 @@
 """
 """
 import numpy as np
+from jax import grad as jax_grad
+from jax import vmap as jax_vmap
+from jax import jit as jax_jit
 from ..sigmoid_smhm import logsm_from_logmhalo_jax, DEFAULT_PARAM_VALUES
 from ..kernel_weighted_hist import triweighted_kernel_histogram_with_derivs as twhist
+from ..sigmoid_smhm import _logsm_from_logmhalo_jax_kern
 
 LOGM = np.linspace(8, 15, 500)
 PARAMS = np.array(list(DEFAULT_PARAM_VALUES.values()))
@@ -23,3 +27,16 @@ def test_kernel_hist_evaluates():
     h, h_jac = twhist(logsm, np.array(jac), logsm_bins, sigma)
     assert h.shape == (logsm_bins.size - 1,)
     assert h_jac.shape == (logsm_bins.size - 1, n_params)
+
+
+def test_kernel_weighted_hist_of_model_gradients():
+    """Compute model gradients with jax and propagate through histogram with numba."""
+    logsm = np.array(logsm_from_logmhalo_jax(LOGM, PARAMS))
+    logsm_bins = np.linspace(10, 11, 10)
+    scatter = 0.25
+    _gradfunc = jax_grad(_logsm_from_logmhalo_jax_kern, argnums=1)
+    gradfunc = jax_jit(jax_vmap(_gradfunc, in_axes=(0, None)))
+    smhm_jac = np.array(gradfunc(LOGM, PARAMS))
+    assert np.shape(smhm_jac) == (LOGM.size, len(PARAMS))
+
+    h, h_jac = twhist(logsm, smhm_jac, logsm_bins, scatter)
